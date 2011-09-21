d59e0bdf8c9173866d51081140f225cd9c34dad8
Ricardo Bánffy <rbanffy@gmail.com>
2011-09-21 16:58:55 -0300

    added extra handlers for initializing the fact database and for doing a single fight

--- src/handlers.py
+++ src/handlers.py
@@ -10,7 +10,8 @@ class MainHandler(webapp.RequestHandler):
     def get(self):
         "The home"
         path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
-        self.response.out.write(template.render(path, {'hello': 'Hello webapp world'}))
+        self.response.out.write(template.render(path, {'hello':
+                                                       'Hello webapp world'}))
 
     def post(self):
         raise NotImplementedError
@@ -34,8 +35,28 @@ class SubmitAFactHandler(webapp.RequestHandler):
         raise NotImplementedError
 
 
-class RandomFightHandler(webapp.RequestHandler):
+def battle(fact1, fact2):
+    # the best fact has 40% chances of having a bad day
+    if fact1.elo_rating > fact2.elo_rating and random.random > .4:
+        fact1.won_over(fact2)
+    else:
+        fact2.won_over(fact1)
+
+
+class ManyFightsHandler(webapp.RequestHandler):
     "Does a couple random fights"
+    def get(self, battles = 10):
+        for i in range(battles):
+            fact1 = Fact.random()
+            fact2 = Fact.random(exclude = [fact1])
+            battle(fact1, fact2)
+
+    def post(self):
+        raise NotImplementedError
+
+
+class InitFactDatabaseHandler(webapp.RequestHandler):
+    "If there are no facts, provide 10 nice ones"
     def get(self, battles = 10):
 
         if Fact.query().count() == 0:
@@ -43,20 +64,17 @@ class RandomFightHandler(webapp.RequestHandler):
             for i in range(10):
                 Fact(text = 'Fact %d' % i).put()
 
-        def battle():
-            fact1 = Fact.random()
-            fact2 = Fact.random(exclude = [fact1])
-            # the best fact has 40% chances of having a bad day
-            if fact1.elo_rating > fact2.elo_rating and random.random > .4:
-                fact1.won_over(fact2)
-            else:
-                fact2.won_over(fact1)
-
-        for i in range(battles):
-            battle()
-
     def post(self):
         raise NotImplementedError
 
+class SingleFightHandler(webapp.RequestHandler):
+    "Does one fight between two random facts"
+    def get(self):
 
+        fact1 = Fact.random()
+        fact2 = Fact.random(exclude = [fact1])
+        battle(fact1, fact2)
+
+    def post(self):
+        raise NotImplementedError
 
